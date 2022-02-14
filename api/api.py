722effed7b023a5e00bf06bdf666c1019672c708
allar14 <93491545+allar14@users.noreply.github.com>
2022-02-14 11:47:15 -0800

    Updated the file - added /users router.

--- api/api.py
+++ api/api.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 
 from api.routes import trackers
 
+from api.routes import users
+
 router = APIRouter()
 
 router.include_router(trackers.router, prefix="/trackers")
+
+router.include_router(users.router, prefix="/users")
