1b95e9c11bd66e1c0b6fbd38aa6d6faae7472af3
Masha Loyanich <masha@soterosoft.com>
2022-02-22 23:54:25 -0800

    fix

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 
 from api.api import router as api_router
@@ -11,6 +10,3 @@ def get_application():
 
 
 app = get_application()
-
-if __name__ == "__main__":
-    uvicorn.run(app)
