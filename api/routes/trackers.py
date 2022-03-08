dcff7c763194e6f171299308313a49ee6ada3a6f
Shondy <olga.tarasova@gmail.com>
2022-03-08 11:56:39 -0800

    deleted commented and duplicate imports, None

--- api/routes/trackers.py
+++ api/routes/trackers.py
@@ -1,9 +1,4 @@
-#from typing import List
 from fastapi import APIRouter, Depends, HTTPException
-import random
-
-# from models.trackers import Tracker, trackers
-from sql_app.models import Tracker
 from sql_app.database import get_db
 from sqlalchemy.orm import Session
 from sql_app import crud, schemas
@@ -16,13 +11,12 @@ router = APIRouter()
 
 @router.get("/", response_model=list[schemas.Tracker])
 def read_trackers_all(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    trackers = crud.get_trackers(db, skip=skip, limit=limit)
-    return trackers
-
+    return crud.get_trackers(db, skip=skip, limit=limit)
+    
 @router.get("/{tracker_id}", response_model=schemas.Tracker)
 def read_tracker_by_id(tracker_id: int, db: Session = Depends(get_db)):
     db_tracker = crud.get_tracker(db, tracker_id=tracker_id)
-    if db_tracker is None:
+    if not db_tracker:
         raise HTTPException(status_code=404, detail="Tracker not found")
     return db_tracker
 
@@ -32,7 +26,7 @@ def read_tracker_by_id(tracker_id: int, db: Session = Depends(get_db)):
 @router.post("/", response_model=schemas.Tracker)
 def create_tracker(tracker: schemas.TrackerCreate, db: Session = Depends(get_db)):
     db_tracker = crud.get_tracker_by_url(db, url_address=tracker.url_address)
-    if db_tracker is not None:
+    if db_tracker:
         raise HTTPException(status_code=400, detail="URL address already registered")
     return crud.create_tracker(db, tracker)
 
@@ -40,7 +34,7 @@ def create_tracker(tracker: schemas.TrackerCreate, db: Session = Depends(get_db)
 @router.put("/{tracker_id}/", response_model=schemas.Tracker)
 def update_tracker(tracker_id: str, tracker: schemas.TrackerBase, db: Session = Depends(get_db)):
     db_tracker = crud.update_tracker(db, tracker_id, tracker)
-    if db_tracker is None:
+    if not db_tracker:
         raise HTTPException(status_code=404, detail="Tracker not found")
     return crud.delete_tracker_by_id(db, tracker_id=tracker_id)
 
@@ -49,6 +43,6 @@ def update_tracker(tracker_id: str, tracker: schemas.TrackerBase, db: Session = 
 @router.delete("/{tracker_id}", response_model=schemas.Tracker)
 def delete_tracker_by_id(tracker_id: str, db: Session = Depends(get_db)):
     db_tracker = crud.delete_tracker_by_id(db, tracker_id=tracker_id)
-    if db_tracker is None:
+    if not db_tracker:
         raise HTTPException(status_code=404, detail="Tracker not found")
     return crud.delete_tracker_by_id(db, tracker_id=tracker_id)
--- api/routes/users.py
+++ api/routes/users.py
@@ -1,12 +1,7 @@
-#from typing import List
 from fastapi import APIRouter, Depends, HTTPException
-from typing import Optional
-# from models.users import User, users
-# from sql_app.models import User
 from sql_app.database import get_db
 from sqlalchemy.orm import Session
 from sql_app import crud, schemas
-from sql_app.database import get_db
 
 router = APIRouter()
 
@@ -15,21 +10,20 @@ router = APIRouter()
 @router.post("/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user is not None:
+    if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db, user)
 
 
 @router.get("/", response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+    return crud.get_users(db, skip=skip, limit=limit)
 
 
 @router.get("/{user_id}", response_model=schemas.User)
 def read_user_by_id(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
+    if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
@@ -37,7 +31,7 @@ def read_user_by_id(user_id: int, db: Session = Depends(get_db)):
 @router.post("/{user_id}/tracker", response_model=schemas.Tracker)
 def create_user_tracker(user_id: int, tracker: schemas.TrackerCreate, db: Session = Depends(get_db)):
     db_tracker = crud.create_user_tracker(db, tracker, user_id)
-    if db_tracker is None:       
+    if not db_tracker:       
         raise HTTPException(status_code=404, detail="User not found")
     # connect to the user in user_tracker
     return db_tracker
