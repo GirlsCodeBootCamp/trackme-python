502921f0c1f6c05199a0c83f3fe21ce2d494a942
Masha Loyanich <masha@soterosoft.com>
2022-03-22 16:32:14 -0700

    proper port

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def get_application():
 
 
 app = get_application()
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=8080)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT"))
