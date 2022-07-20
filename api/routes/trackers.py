91be98a6e46f154cbf05e24c20abde3ea318ed46
Masha Loyanich <masha@soterosoft.com>
2022-07-19 21:03:22 -0700

    tracking changes as a background task

--- api/routes/trackers.py
+++ api/routes/trackers.py
@@ -1,12 +1,19 @@
 from typing import Optional
 
 from api.routes.utils import VerifyToken
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from models import trackers
 from sql_app import crud
 from sql_app.database import get_db
 from sqlalchemy.orm import Session
+import time
+import difflib
+
+import urllib3
+from bs4 import BeautifulSoup as bs
+import re
+
 
 router = APIRouter()
 token_auth_scheme = HTTPBearer()
@@ -70,3 +77,42 @@ def delete_tracker_by_id(tracker_id: int, db: Session = Depends(get_db)):
     if not db_tracker:
         raise HTTPException(status_code=404, detail="Tracker not found")
     return crud.delete_tracker_by_id(db, tracker_id=tracker_id)
+
+
+def check_tracker(tracker: str, db):
+    print(tracker.id, tracker.url_address)
+    old_content = tracker.content
+
+    http = urllib3.PoolManager()
+    try:
+        resp = http.request("GET", tracker.url_address)
+        root = resp.data
+        soup = bs(root)  # make BeautifulSoup
+        output = soup.get_text()
+        new_content = re.sub(r"\n+", "\n", output)
+        changed = False
+        if tracker.content:
+            i = 0
+            for text in difflib.unified_diff(new_content.split("\n"), old_content.split("\n")):
+                if text[:3] not in ("+++", "---", "@@ "):
+                    changed = True
+                    print(text)
+                    i += 1
+        if changed or not tracker.content:
+            tracker.content = new_content
+            crud.update_tracker(db, tracker.id, tracker)
+            # TODO Add timestamp when changed
+            # TODO send an email, add task to background task?
+            # TODO add to UI and/or API validation of the URL when creating a tracker
+    except Exception:
+        print("URL not valid")
+
+# testing endpoint
+@router.post("/track")
+async def send_notification(
+    background_tasks: BackgroundTasks, db: Session = Depends(get_db)
+):
+    result = crud.get_trackers(db)
+    for tr in result:
+        background_tasks.add_task(check_tracker, tr, db)
+    return result
--- sql_app/models.py
+++ sql_app/models.py
@@ -38,5 +38,6 @@ class Tracker(Base):
     name = Column(String)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     deleted = Column(Boolean, default=False)
+    content = Column(String)
 
     owners = relationship("User", secondary=user_tracker, back_populates="trackers")
